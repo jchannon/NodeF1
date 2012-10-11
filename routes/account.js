e251e8a582bf6bebbe0051d648c62b7b73569ee2
Jonathan Channon <jonathan.channon@gmail.com>
2012-10-11 23:04:11 +0100

    Moved page title away from View Model into Jade block

--- routes/account.js
+++ routes/account.js
@@ -2,13 +2,11 @@
 
 exports.login = function(req, resp) {
 	resp.render("login/login", {
-		title: "Login",
 		currentUrl: req.path
 	});
 };
 
-exports.loginuser= function(req,resp)
-{
-console.log(req.body.username + req.body.password);
-resp.send(req.body.username + req.body.password);
+exports.loginuser = function(req, resp) {
+	console.log(req.body.username + req.body.password);
+	resp.send(req.body.username + req.body.password);
 };
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,13 @@ var request = require('request'),
 	http = require('http');
 
 exports.currentDriverStandings = function(req, response) {
-
-
+	
 	var date = new Date();
 	var year = date.getUTCFullYear();
 
 	request.get("http://ergast.com/api/f1/" + year + "/driverStandings.json", function(err, res, body) {
 		if(!err) {
-			buildData('current/index', 'Current Driver Standings', body, req, response);
+			buildData('current/index', body, req, response);
 		}
 	});
 
@@ -24,7 +23,7 @@ exports.currentConstructorStandings = function(req, response) {
 
 	request.get("http://ergast.com/api/f1/" + year + "/5/constructorStandings.json", function(err, res, body) {
 		if(!err) {
-			buildData('current/constructorstandings', 'Current Constructor Standings', body, req, response);
+			buildData('current/constructorstandings', body, req, response);
 		}
 	});
 
@@ -40,7 +39,6 @@ exports.currentSchedule = function(req, response) {
 			var model = JSON.parse(body);
 
 			response.render("current/currentSchedule", {
-				title: "Current Race Schedule",
 				currentUrl: req.path,
 				RaceData: model.MRData.RaceTable
 			});
@@ -49,11 +47,10 @@ exports.currentSchedule = function(req, response) {
 
 };
 
-function buildData(url, title, body, req, response) {
+function buildData(url,  body, req, response) {
 
 	var model = JSON.parse(body);
 	response.render(url, {
-		title: title,
 		currentUrl: req.path,
 		RaceData: model.MRData.StandingsTable
 	});
