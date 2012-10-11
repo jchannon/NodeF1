821966f2736d34982a6bee6541585c149656f979
Jonathan Channon <jonathan.channon@gmail.com>
2012-10-11 11:55:20 +0100

    made ST2 project changes added basic login routes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 "use strict";
 
 var express = require('express'),
-    home = require('./routes/index.js'),
-    user = require('./routes/user.js'),
-    http = require('http'),
-    path = require('path');
+  home = require('./routes/index.js'),
+  user = require('./routes/user.js'),
+  http = require('http'),
+  path = require('path'),
+  account = require('./routes/account.js');
 
 var app = express();
 
-app.configure(function(){
+app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -20,16 +21,17 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function() {
+  app.use(express.errorHandler());   
 });
 
 app.get('/', home.currentDriverStandings);
 app.get('/constructor-standings', home.currentConstructorStandings);
 app.get('/current-schedule', home.currentSchedule);
+app.get('/login', account.login);
+app.post('/loginuser', account.loginuser);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
- 
